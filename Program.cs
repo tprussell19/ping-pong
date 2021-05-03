cd30a5419126d67519a180682489b784044ac3bf
Thomas Russell <t.p.russell19@gmail.com>
2021-05-03 13:30:03 -0700

    add code to succesfully run ping-pong console application
    co-authored-by: Jesse White <jesse.white6@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ping_pong
+
+public class Program
 {
-  class Program
+  public static void Main()
   {
-    static void Main()
+    Console.WriteLine("Please enter a number you would like to count up to for the game to begin: ");
+    string userInput = Console.ReadLine();
+    int parsedInput = int.Parse(userInput);
+
+    Console.WriteLine("Your game is now starting:");
+    for (int index = 1; index <= parsedInput; index++)
     {
-      ;
+      if (index % 3 == 0 && index % 5 == 0)
+      {
+        Console.WriteLine("ping-pong");
+      }
+      else if (index % 3 == 0)
+      {
+        Console.WriteLine("ping");
+      }
+      else if (index % 5 == 0)
+      {
+        Console.WriteLine("pong");
+      }
+      else
+      {
+        Console.WriteLine(index);
+      }
     }
   }
 }
+
